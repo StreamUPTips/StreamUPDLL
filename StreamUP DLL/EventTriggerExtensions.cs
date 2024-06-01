9f24371aa324c15d13a2473ec0dec2ea6abd560e
TerrierDarts <82717017+TerrierDarts@users.noreply.github.com>
2024-06-01 15:28:20 +0100

    Currency Added

--- StreamUP DLL/EventTriggerExtensions.cs
+++ StreamUP DLL/EventTriggerExtensions.cs
@@ -9,6 +9,7 @@ using System.Diagnostics;
 using System.Text;
 using Streamer.bot.Plugin.Interface.Model;
 using System.Globalization;
+using Streamer.bot.Plugin.Interface.Enums;
 
 namespace StreamUP {
 
@@ -605,6 +606,67 @@ namespace StreamUP {
             CPH.SendYouTubeMessage(message, botAccount);
             //CPH.SendTrovoMessage(message, botAccount);
         }
+
+        public static void SUSBSendPlatformMessage(this IInlineInvokeProxy CPH, string platform, string message, bool botAccount)
+        {
+            if(platform == "twitch")
+            {
+                CPH.SendMessage(message, botAccount);
+            }
+            if(platform == "youtube")
+            {
+                CPH.SendYouTubeMessage(message, botAccount);
+            }
+        }
+
+        
+        public static string SURawInputWithInputsRemoved(this IInlineInvokeProxy CPH, int inputsToRemove)
+        {
+            string textRemove = "";
+            for (int i = inputsToRemove; CPH.TryGetArg("input" + i, out string moreInput); i++)
+            {
+                textRemove += moreInput;
+            }
+            return textRemove;
+        }
+
+          public static bool SUSBRefund(this IInlineInvokeProxy CPH, bool refund = false)
+        {
+            CPH.TryGetArg("rewardId", out string reward);
+            CPH.TryGetArg("redemptionId", out string redemption);
+            if (reward == null || redemption == null)
+            {
+                return true;
+            }
+            if (refund)
+            {
+
+                CPH.TwitchRedemptionCancel(reward, redemption);
+            }
+
+            return true;
+        }
+
+          public static bool SUSBFulfill(this IInlineInvokeProxy CPH, bool fulfill = false)
+        {
+
+           
+            CPH.TryGetArg("rewardId", out string reward);
+            CPH.TryGetArg("redemptionId", out string redemption);
+            if (reward == null || redemption == null)
+            {
+                return true;
+            }
+            if (fulfill)
+            {
+
+                CPH.TwitchRedemptionFulfill(reward, redemption);
+            }
+
+            return true;
+        }
+
+
 
         public static T SUSBTryGetArgOrDefault<T>(this IInlineInvokeProxy CPH, string key, T defaultValue = default)
         {
