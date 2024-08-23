932e818387659c8dcc32bc65f94acadc9aae22c2
WaldoAndFriends <hello@waldoandfriends.tv>
2024-08-23 15:55:19 +0200

    Build move over to modern .csproj

--- StreamUP DLL/Properties/AssemblyInfo.cs
+++ StreamUP DLL/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Minor Version
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("1.0.11.11")]
-[assembly: AssemblyFileVersion("1.0.11.11")]
+[assembly: AssemblyVersion("1.0.11.12")]
+[assembly: AssemblyFileVersion("1.0.11.12")]
