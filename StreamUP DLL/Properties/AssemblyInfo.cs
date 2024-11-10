d2c6bb5351a53d7fb3153bb55025ec23725863ad
TerrierDarts <82717017+TerrierDarts@users.noreply.github.com>
2024-11-10 16:52:45 +0000

    Initial Push

--- StreamUP DLL/Properties/AssemblyInfo.cs
+++ StreamUP DLL/Properties/AssemblyInfo.cs
@@ -22,7 +22,7 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("38806bcb-0167-4a7e-ac8e-ede4e72f380c")]
 
-// Version information for an assembly consists of the following four values:
+// Version information for an assembly consists of the following four values:g
 //
 //      Major Version
 //      Minor Version
