f1c6cc3d01aa9266d996bf5a01dd864fb19c1a05
TerrierDarts <82717017+TerrierDarts@users.noreply.github.com>
2024-11-10 16:57:24 +0000

    Update AssemblyInfo.cs

--- StreamUP DLL/Properties/AssemblyInfo.cs
+++ StreamUP DLL/Properties/AssemblyInfo.cs
@@ -22,11 +22,11 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("38806bcb-0167-4a7e-ac8e-ede4e72f380c")]
 
-// Version information for an assembly consists of the following four values:g
+// Version information for an assembly consists of the following four values:
 //
 //      Major Version
 //      Minor Version
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("1.0.11.14")]
-[assembly: AssemblyFileVersion("1.0.11.14")]
+[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
