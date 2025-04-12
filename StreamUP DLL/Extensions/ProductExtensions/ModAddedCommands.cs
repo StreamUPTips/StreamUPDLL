cecd0a2a5d91b4d105c4046637f78e2a944f8c20
TerrierDarts <82717017+TerrierDarts@users.noreply.github.com>
2025-04-12 14:49:22 +0100

    Mod Added Triggers Expanded

--- StreamUP DLL/Extensions/ProductExtensions/ModAddedCommands.cs
+++ StreamUP DLL/Extensions/ProductExtensions/ModAddedCommands.cs
@@ -22,15 +22,30 @@ namespace StreamUP
     public void SetTriggersForModAddedCommands()
     {
       string[] commands = { "StreamUP", "Mod Added Commands", "Commands" };
+      string[] counts = { "StreamUP", "Mod Added Commands", "Commands" };
       List<CustomTrigger> customTriggers = new List<CustomTrigger>
             {
                 new("Command Triggered", "macCommandTriggered", commands),
                 new("Command Added", "macCommandAdded", commands),
                 new("Command Edited", "macCommandEdited", commands),
                 new("Command Deleted", "macCommandDeleted", commands),
-               
+                new("Command Fail", "macCommandFailed", commands),
+                new("Count Failed", "macCountFailed", counts),
+                new("Count Updated", "macCountUpdated", counts),
+                new("Count Check", "macCountCheck", counts),
+
+
             };
       SetCustomTriggers(customTriggers);
+    }
+
+    public bool ModAddedCommandFail(int code, string message)
+    {
+      _CPH.SetArgument("errorCode", code);
+      _CPH.SetArgument("errorMessage", message);
+      LogError($"{code} - {message}");
+      _CPH.TriggerCodeEvent("macCommandFailed");
+      return true;
     }
   }
 
