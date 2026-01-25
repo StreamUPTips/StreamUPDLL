467174ffbfb2d7da9fce8f73c2a8db30dad95c99
TerrierDarts <82717017+TerrierDarts@users.noreply.github.com>
2026-01-25 14:56:23 +0000

    UserPortalStuffs

--- StreamUP DLL/Extensions/ProductExtensions/UserPortal.cs
+++ StreamUP DLL/Extensions/ProductExtensions/UserPortal.cs
@@ -56,5 +56,52 @@ namespace StreamUP
             var response = await _httpClient.PostAsync($"https://user.streamup.tips/{broadcasterId}/death_counter", requestContent);
             return response.IsSuccessStatusCode;
         }
+
+        //! TESTING ACTIONS INTERNAL USE ONLY
+        public async Task<bool> SendTestDataToQuotes(string apiKey, string broadcasterId, object content, string port = "5221")
+        {
+            var json = JsonConvert.SerializeObject(content);
+            using var requestContent = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+            requestContent.Headers.Add("X-API-KEY", apiKey);
+            var response = await _httpClient.PostAsync($"http://localhost:{port}/{broadcasterId}/quotes", requestContent);
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> SendTestDataToCommands(string apiKey, string broadcasterId, object content, string port = "5221")
+        {
+            var json = JsonConvert.SerializeObject(content);
+            using var requestContent = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+            requestContent.Headers.Add("X-API-KEY", apiKey);
+            var response = await _httpClient.PostAsync($"http://localhost:{port}/{broadcasterId}/commands", requestContent);
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> SendTestDataToBitMenu(string apiKey, string broadcasterId, object content, string port = "5221")
+        {
+            var json = JsonConvert.SerializeObject(content);
+            using var requestContent = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+            requestContent.Headers.Add("X-API-KEY", apiKey);
+            var response = await _httpClient.PostAsync($"http://localhost:{port}/{broadcasterId}/bitmenu", requestContent);
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> SendTestDataToDeathCounter(string apiKey, string broadcasterId, object content, string port = "5221")
+        {
+            var json = JsonConvert.SerializeObject(content);
+            using var requestContent = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+            requestContent.Headers.Add("X-API-KEY", apiKey);
+            var response = await _httpClient.PostAsync($"http://localhost:{port}/{broadcasterId}/death_counter", requestContent);
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> SendTestPoints(string apiKey, string broadcasterId, object content, string port = "5221")
+        {
+            var json = JsonConvert.SerializeObject(content);
+            using var requestContent = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+            requestContent.Headers.Add("X-API-KEY", apiKey);
+            var response = await _httpClient.PostAsync($"http://localhost:{port}/{broadcasterId}/leaderboard", requestContent);
+            return response.IsSuccessStatusCode;
+        }
+
     }
 }
